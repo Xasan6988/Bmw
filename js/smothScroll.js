033900054241408c3b946fd66ab2e62a44d3e743
Atryom <238136@mail.ru>
2021-05-20 11:57:23 +0300

    Lesson 2 + optional task

---
+++ js/smothScroll.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const smothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+
+  for (let elem of smothScrollElems) {
+    elem.addEventListener('click', function (event) {
+			event.preventDefault();
+			const id = elem.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		});
+  }
+})
---
+++ js/tabs.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
+  const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+  const tabsTitles = document.querySelectorAll('.design__title');
+
+  for (let elem of tabsHandlerElems) {
+    elem.addEventListener('click', function () {
+      tabsHandlerElems.forEach(item => {
+        if (elem === item) {
+          item.classList.add('design-list__item_active');
+        } else {
+          item.classList.remove('design-list__item_active');
+        }
+      });
+
+      tabsFieldElems.forEach(item => {
+        if (item.dataset.tabsField === elem.dataset.tabsHandler) {
+          item.classList.remove('hidden');
+        } else {
+          item.classList.add('hidden');
+        };
+      });
+
+      tabsTitles.forEach(item => {
+          if (item.dataset.title === elem.dataset.tabsHandler) {
+            item.classList.add('hidden');
+          } else {
+            item.classList.remove('hidden');
+          }
+      });
+    });
+  };
+});
