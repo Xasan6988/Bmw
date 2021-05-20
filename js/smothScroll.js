0710835afd7bf03a6e66449feaeb9536255bea85
Atryom <238136@mail.ru>
2021-05-20 14:46:53 +0300

    Fix

--- js/smothScroll.js
+++ js/smothScroll.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const smothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
   for (let elem of smothScrollElems) {
-    elem.addEventListener('click', function (event) {
+    elem.addEventListener('click', function(event) {
 			event.preventDefault();
 			const id = elem.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
